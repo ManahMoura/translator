d55bc78609e128cfd7c1d92476a6e0b71fb9f076
ManahMoura <manamiui2001@gmail.com>
2020-03-07 19:10:56 -0300

    project ok

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,45 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		List<String> list = new ArrayList<>();
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("CAMINHO PARA CRIAÇÃO DA PASTA: ");
+		String path = sc.nextLine();
+
+		System.out.println("PALAVRA EM INGLÊS: ");
+		String word = sc.nextLine();
+
+		System.out.println("QUANTIDADE DE REPETEIÇÕES: ");
+		Integer rep = sc.nextInt();
+		sc.nextLine();
+		
+		for (int i = 1; i <= rep; i++) {
+			System.out.println(i + "ª PALAVRA: ");
+			String words = sc.nextLine();
+			list.add(words);
+		}
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			for (String line : list) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR!!! ==> " + e.getMessage());
+		}
+
+		sc.close();
 	}
-
 }
